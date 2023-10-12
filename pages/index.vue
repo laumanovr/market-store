7fd48b71728c0b6b3f45c02b8cbb3bd8d8a52f80
xbaysal11 <baisalbek.daniiarov@gmail.com>
2023-10-12 16:46:13 +0600

    ADD home page, terms conditions page, privacy policy page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,37 @@
 <template>
-  <div class="message">
-    <span>soon</span>
+  <div class="layout">
+    <div class="home">
+      <div class="home-data">
+        <div class="home-title">
+          <h1>Ogogo – Ваша площадка для <br />вау-покупок</h1>
+        </div>
+        <div class="home-text">
+          <p>
+            Скоро наш новый сервис станет доступным для вас! Следите за нашими
+            обновлениями и готовьтесь к захватывающим покупкам в рассрочку!
+          </p>
+        </div>
+      </div>
+      <div class="home-banner"><img :src="homeBanner" alt="Home banner" /></div>
+    </div>
+    <div class="links">
+      <div class="link">
+        <NuxtLink to="/privacy-policy">Privacy Policy</NuxtLink>
+      </div>
+      <div class="link">
+        <NuxtLink to="/terms-conditions">Terms Conditions</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-.message {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
-</style>
+<script lang="ts">
+import homeBanner from "../public/images/home-banner.png";
+export default {
+  data() {
+    return {
+      homeBanner,
+    };
+  },
+};
+</script>
