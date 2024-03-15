9eaf614cbcc05a882599ba53907589caf129eac5
Ramazan <laumanov.r@gmail.com>
2024-03-15 10:43:07 +0600

    import profile

--- pages/dev.vue
+++ pages/dev.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <Header />
+    <Profile />
+    <hr />
+    <ProductCard />
   </div>
 </template>
 
 <script lang="ts" setup>
 import Header from "../shared/header/Header.vue";
+import Profile from "../shared/profile/Profile.vue";
 </script>
