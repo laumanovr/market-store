7d532c13452556c206aabd3fc99abbbd98ad9ae5
Ramazan <laumanov.r@gmail.com>
2024-03-15 10:43:40 +0600

    import product-card

--- pages/dev.vue
+++ pages/dev.vue
@@ -10,4 +10,5 @@
 <script lang="ts" setup>
 import Header from "../shared/header/Header.vue";
 import Profile from "../shared/profile/Profile.vue";
+import ProductCard from "../shared/product-card/ProductCard.vue";
 </script>
