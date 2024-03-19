d46617b832f89f5773ad78a8ae61631b187dc2d9
Ramazan <laumanov.r@gmail.com>
2024-03-19 12:25:42 +0600

    set layout

---
+++ pages/profile.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Profile />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Profile } from "~/widgets/profile";
+definePageMeta({
+  layout: "main",
+});
+</script>
