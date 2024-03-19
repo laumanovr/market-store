5e28aaa14c76ebe4afea4fddfef70c6bbb71b4ad
Ramazan <laumanov.r@gmail.com>
2024-03-19 12:25:22 +0600

    route-component for cart

---
+++ pages/cart.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="cart-content">
+    <Cart />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Cart } from "~/widgets/cart";
+definePageMeta({
+  layout: "main",
+});
+</script>
