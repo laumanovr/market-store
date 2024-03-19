c18f39438a188f5ff05e181fa654ed64b130528e
Ramazan <laumanov.r@gmail.com>
2024-03-19 12:25:10 +0600

    route-component for favorite

---
+++ pages/favorite.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="favorite">
+    <Favorite />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Favorite } from "~/widgets/favorite";
+definePageMeta({
+  layout: "main",
+});
+</script>
