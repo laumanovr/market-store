63a4650811cf712e1571723c944ad171f1f2638d
Ramazan <laumanov.r@gmail.com>
2024-03-19 12:22:43 +0600

    create main layout

---
+++ layouts/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-layout">
+    <Header />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Header from "../shared/components/header/Header.vue";
+</script>
+
+<style>
+.main-layout {
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 0 40px 40px;
+}
+</style>
