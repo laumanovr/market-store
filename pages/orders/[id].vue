b10c778e2796629b3fb514e9f5cb3f2cf19a9bd2
Ramazan <laumanov.r@gmail.com>
2024-03-20 15:15:00 +0600

    create order-detail page

---
+++ pages/orders/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="order-detail-container">
+    <OrderDetail />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { OrderDetail } from "~/widgets/orders";
+definePageMeta({
+  layout: "main",
+});
+</script>
