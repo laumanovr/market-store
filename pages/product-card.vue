626a4014d8199a9b99b2f57b6542accd18d6f5b8
@lisovSergei <solohad1n@gmail.com>
2024-03-20 12:55:57 +0600

    add Breadcrumbs

---
+++ pages/product-card.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="product-card__content">
+        <ProductCard/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ProductCard } from "~/widgets/ProductCard";
+definePageMeta({
+  layout: "main",
+});
+</script>
---
+++ widgets/ProductCard/index.ts
@@ -0,0 +1,2 @@
+import ProductCard from "./ui/Product-card.vue";
+export { ProductCard };
