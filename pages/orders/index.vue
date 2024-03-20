7e1d17e6ecd0207ff07e0eed8027d33db0b7eb52
Ramazan <laumanov.r@gmail.com>
2024-03-20 15:10:09 +0600

    moved order component to folder

---
+++ pages/orders/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Orders />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Orders } from "~/widgets/orders";
+definePageMeta({
+  layout: "main",
+});
+</script>
