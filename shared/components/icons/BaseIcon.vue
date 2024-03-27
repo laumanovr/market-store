308a341b08d06ca38c8f0b3c5cf3652e3b9c3363
@lisovSergei <solohad1n@gmail.com>
2024-03-27 10:26:14 +0600

    feat: add Icon components

---
+++ shared/components/icons/BaseIcon.vue
@@ -0,0 +1,31 @@
+<template>
+    <svg
+        :class="$attrs.class || 'icon'"
+        :viewBox="viewBox || '0 0 16 16'"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        v-html="path"
+    >
+    </svg>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import icons from './icons'
+
+interface Props {
+    name: keyof typeof icons;
+    viewBox?: string;
+}
+
+const { name, viewBox } = defineProps<Props>()
+
+const path = ref(icons[name])
+</script>
+
+<style lang="scss" scoped>
+    .icon {
+        width: 16px;
+        height: 16px;
+    }
+</style>
--- widgets/main/ui/Main.vue
+++ widgets/main/ui/Main.vue
@@ -16,9 +16,7 @@
                 <Button color="gray">
                   Все товары
                   <template v-slot:rightIcon>
-                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M3.5 1.5L7.89496 5.75492C7.92825 5.78708 7.95467 5.82528 7.9727 5.86733C7.99072 5.90938 8 5.95447 8 6C8 6.04553 7.99072 6.09062 7.9727 6.13267C7.95467 6.17472 7.92825 6.21292 7.89496 6.24508L3.5 10.5" stroke="#A5ABB8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                    <BaseIcon name="arrow" viewBox="0 0 14 11"/>
                   </template>
                 </Button>
               </div>
@@ -43,9 +41,7 @@
                 <Button color="gray">
                   Все товары
                   <template v-slot:rightIcon>
-                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M3.5 1.5L7.89496 5.75492C7.92825 5.78708 7.95467 5.82528 7.9727 5.86733C7.99072 5.90938 8 5.95447 8 6C8 6.04553 7.99072 6.09062 7.9727 6.13267C7.95467 6.17472 7.92825 6.21292 7.89496 6.24508L3.5 10.5" stroke="#A5ABB8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                    <BaseIcon name="arrow" viewBox="0 0 14 11"/>
                   </template>
                 </Button>
               </div>
@@ -70,9 +66,7 @@
                 <Button color="gray">
                   Все товары
                   <template v-slot:rightIcon>
-                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M3.5 1.5L7.89496 5.75492C7.92825 5.78708 7.95467 5.82528 7.9727 5.86733C7.99072 5.90938 8 5.95447 8 6C8 6.04553 7.99072 6.09062 7.9727 6.13267C7.95467 6.17472 7.92825 6.21292 7.89496 6.24508L3.5 10.5" stroke="#A5ABB8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                    <BaseIcon name="arrow" viewBox="0 0 14 11"/>
                   </template>
                 </Button>
               </div>
@@ -101,6 +95,7 @@ import Slider from '~/shared/components/slider/Slider.vue'
 import { Promotions } from '~/widgets/promotions'
 import ProductCard from '~/shared/components/product-card/ProductCard.vue'
 import Button from '~/shared/components/button/button.vue';
+import BaseIcon from '~/shared/components/icons/BaseIcon.vue';
 
 const sliderItems = ref<{ id: number; name: string; img: string; }[]>([
   {
