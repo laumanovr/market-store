76459893bb3f104e0ca5a5c8744bf9091e96b45a
Ramazan <laumanov.r@gmail.com>
2024-06-13 17:56:51 +0600

    modified main page

--- widgets/main/ui/Main.vue
+++ widgets/main/ui/Main.vue
@@ -13,12 +13,10 @@
             <div class="product-card__title">
               <h3>Новинки месяца</h3>
               <div class="product-card__btn">
-                <Button color="gray">
-                  Все товары
-                  <template v-slot:rightIcon>
-                    <BaseIcon name="arrow" viewBox="0 0 14 11" />
-                  </template>
-                </Button>
+                <SButton type="secondary" size="small">
+                  <span class="s-mr-2 s-text-tertiary-dark">Все товары</span>
+                  <SIconRender name="ArrowIcon" direction="right" />
+                </SButton>
               </div>
             </div>
             <div class="product-card__content">
@@ -39,12 +37,10 @@
             <div class="product-card__title">
               <h3>Рекомендуем приобрести</h3>
               <div class="product-card__btn">
-                <Button color="gray">
-                  Все товары
-                  <template v-slot:rightIcon>
-                    <BaseIcon name="arrow" viewBox="0 0 14 11" />
-                  </template>
-                </Button>
+                <SButton type="secondary" size="small">
+                  <span class="s-mr-2 s-text-tertiary-dark">Все товары</span>
+                  <SIconRender name="ArrowIcon" direction="right" />
+                </SButton>
               </div>
             </div>
             <div class="product-card__content">
@@ -65,12 +61,10 @@
             <div class="product-card__title">
               <h3>Мега Кешбэк</h3>
               <div class="product-card__btn">
-                <Button color="gray">
-                  Все товары
-                  <template v-slot:rightIcon>
-                    <BaseIcon name="arrow" viewBox="0 0 14 11" />
-                  </template>
-                </Button>
+                <SButton type="secondary" size="small">
+                  <span class="s-mr-2 s-text-tertiary-dark">Все товары</span>
+                  <SIconRender name="ArrowIcon" direction="right" />
+                </SButton>
               </div>
             </div>
             <div class="product-card__content">
@@ -94,12 +88,11 @@
 </template>
 
 <script lang="ts" setup>
+import { SButton, SIconRender } from '@tumarsoft/ogogo-ui'
 import { ref } from 'vue'
 import Slider from '~/shared/components/slider/Slider.vue'
 import { Promotions } from '~/widgets/promotions'
 import ProductCard from '~/shared/components/product-card/ProductCard.vue'
-import Button from '~/shared/components/button/button.vue'
-import BaseIcon from '~/shared/components/icons/BaseIcon.vue'
 const router = useRouter()
 
 const sliderItems = ref<{ id: number; name: string; img: string }[]>([
@@ -141,7 +134,6 @@ const sliderItems = ref<{ id: number; name: string; img: string }[]>([
     display: flex;
     align-items: center;
     justify-content: flex-start;
-    flex-wrap: wrap;
     grid-gap: 15px;
     margin-top: 36px;
   }
@@ -170,13 +162,13 @@ const sliderItems = ref<{ id: number; name: string; img: string }[]>([
   &__btn {
     width: 140px;
   }
-  &__btn:deep(.button) {
+  &__btn:deep(.SButton) {
     height: 40px;
     color: $gray-500;
     display: flex;
     align-items: center;
   }
-  &__btn:deep(.button__text) {
+  &__btn:deep(.SButton__text) {
     margin-right: 8px;
     font-size: 14px;
     font-weight: 600;
