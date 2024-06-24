f0ef62f2e083ab19a274fb9fbd1341a62532d4bc
Ramazan <laumanov.r@gmail.com>
2024-06-24 10:49:01 +0600

    create axios

--- shared/plugins/axios.ts
+++ shared/plugins/axios.ts
@@ -2,3 +2,20 @@ import axios from 'axios'
 const config = useRuntimeConfig()
 
 const baseURL = config.public.API_BASE_URL
+
+export const API = axios.create({
+  baseURL,
+})
+
+// API.interceptors.request.use(
+//     function (config) {
+//       const authStore = useAuthStore();
+//       const token = authStore.getSessionId;
+//       config.headers.Authorization = token ? `Bearer ${token}` : "";
+
+//       return config;
+//     },
+//     function (error) {
+//       return Promise.reject(error);
+//     }
+//   );
