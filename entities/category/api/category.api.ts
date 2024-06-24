4e5fa2e5e83c79aa35a38ae12f3bf5dabfacb9b4
Ramazan <laumanov.r@gmail.com>
2024-06-24 10:48:23 +0600

    create cateogry-api

---
+++ entities/category/api/category.api.ts
@@ -0,0 +1,12 @@
+import { API } from '~/shared/plugins/axios'
+
+export class CategoryApi {
+  // getAllCategories = () => {
+  //   return API({
+  //     method: 'GET',
+  //     url: 'Marketplace/GetMarketplaceCategories',
+  //   })
+  //     .then(response => response.data)
+  //     .catch(err => Promise.reject(err))
+  // }
+}
