170cd5a1b55dbbdf7d010694aa8b553aa6810e22
Ramazan <laumanov.r@gmail.com>
2024-06-27 15:13:59 +0600

    add responsive banner

--- widgets/promotions/ui/Promotions.vue
+++ widgets/promotions/ui/Promotions.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="promotions">
-    <div class="promotions__container">
-      <div class="promotions__content">
-        <div class="promotions__image">
-          <img src="~/assets/images/banner.png" alt="" />
+  <div class="promotions-container">
+    <div class="promotions" v-for="item in 5" :key="item">
+      <div class="promotions__container">
+        <div class="promotions__content">
+          <div class="promotions__image">
+            <img src="~/assets/images/banner.png" alt="" />
+          </div>
         </div>
       </div>
     </div>
@@ -13,16 +15,28 @@
 <script lang="ts" setup></script>
 
 <style lang="scss" scoped>
-.promotions {
-  &__image {
-    min-width: 158px;
-    max-width: 260px;
-    min-height: 158px;
-    max-height: 260px;
-  }
-  &__image img {
+@import '~/assets/style/screens.scss';
+.promotions-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  grid-gap: 15px;
+  margin-top: 36px;
+  @media #{$sm} {
     width: 100%;
-    height: 100%;
+    overflow-x: auto;
+  }
+  .promotions {
+    &__image {
+      min-width: 158px;
+      max-width: 260px;
+      min-height: 158px;
+      max-height: 260px;
+    }
+    &__image img {
+      width: 100%;
+      height: 100%;
+    }
   }
 }
 </style>
